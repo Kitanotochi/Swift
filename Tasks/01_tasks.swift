870ad29ab2ffad9857ca9743313c0cf40b22b15c
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-04-25 20:38:05 +0300

    Create 01_tasks.swift

---
+++ Tasks/01_tasks.swift
@@ -0,0 +1,10 @@
+/*
+Объявите две строковые константы `firstString` и `secondString`. 
+Присвойте им значения "I can" и "code". 
+Выведите в консоль фразу "I can code!" используя эти строковые переменные.
+*/
+
+let firstString = "I can"
+let secondString = "code!"
+print("\(firstString) \(secondString)")
+
