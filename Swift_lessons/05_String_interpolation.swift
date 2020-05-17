2b40acee28166fe7d4dadee792ed226ebbeac251
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-05-18 01:12:44 +0300

    Create 05_String_interpolation.swift

---
+++ Swift_lessons/05_String_interpolation.swift
@@ -0,0 +1,6 @@
+// Интерполяция строки
+
+// Интерполяция - позволяет вставлять значения констант или переменных в строку
+let temperature = 12
+let windSpeed = 5
+let weatherDescription = "Temperature is about \(temperature) above zero and wind speed is about \(windSpeed) m/s"
