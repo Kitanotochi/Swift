8163ac3c34519592f86cb3c4eb0c5b69477ce839
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-05 20:40:10 +0300

    Update 10_Guard_Statement.swift

--- Swift_lessons/10_Guard_Statement.swift
+++ Swift_lessons/10_Guard_Statement.swift
@@ -1,1 +1,29 @@
 // Инструкция Guard в Swift
+
+func someFunc(a: Int, b: Int) {
+    // Выполняется код
+    guard a == b else { return }
+    // Выполняется код
+    guard b == 11 else { return }
+    
+        // Выполняется сод
+if a == b {
+    // Выполняется код
+    if b == 11 {
+        
+    }
+  }
+}
+
+// Пример инструкции "guard" в цикле
+for i in 1...5 {
+    guard i != 3 else { break }
+    print(i)
+}
+
+
+for i in 1...5 {
+    if i == 3 {
+        print(i)
+    }
+}
