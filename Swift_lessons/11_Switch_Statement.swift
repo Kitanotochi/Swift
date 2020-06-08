8debadfe34a641754993a444a3c5a857cda7dbc3
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-08 16:50:39 +0300

    Update 11_Switch_Statement.swift

--- Swift_lessons/11_Switch_Statement.swift
+++ Swift_lessons/11_Switch_Statement.swift
@@ -9,3 +9,17 @@ if totalScore == 10 {
 } else if totalScore == 100 {
     print("The number is 100")
 }
+
+// Пример. Альтернатива инструкции "if"
+switch totalScore {
+case 10:
+    print("The number is 10")
+case 20:
+    print("The number is 20")
+case 50..<100:
+    print("The number is from 50 to 100")
+case 100:
+    print("The number is 100")
+default:
+    break
+}
