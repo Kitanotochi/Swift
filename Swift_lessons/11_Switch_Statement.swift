2e848e0f13f8ad4e40f4638eb26295699335f124
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-08 16:49:55 +0300

    Update 11_Switch_Statement.swift

--- Swift_lessons/11_Switch_Statement.swift
+++ Swift_lessons/11_Switch_Statement.swift
@@ -1,1 +1,11 @@
 // Инструкция Switch в Swift
+
+// Пример инструкции "if"
+let totalScore = 20
+if totalScore == 10 {
+    print("The number is 10")
+} else if totalScore == 20 {
+    print("The number is 20")
+} else if totalScore == 100 {
+    print("The number is 100")
+}
