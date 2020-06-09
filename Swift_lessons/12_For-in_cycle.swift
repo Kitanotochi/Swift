4eb6d2c7834fae445a8e2f21ba2367a38dc0e42f
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-09 19:08:36 +0300

    Update 12_For-in_cycle.swift

--- Swift_lessons/12_For-in_cycle.swift
+++ Swift_lessons/12_For-in_cycle.swift
@@ -1,1 +1,25 @@
 // Цикл for-in в Swift
+
+// Пример цикла
+// Массив, как и диапозон рассматриваются одинаково при работе с циклами
+let array = [1, 2, 3, 4, 5] // Создали массив
+for index in array {
+    print(index)
+}
+
+// Пример работы цикла со словарем
+let nameAndAge = ["Ivan": 20, "Oleg": 18, "Andrei": 15]
+for (name, numberOfAge) in nameAndAge { // Перебор словаря по ключу и значению
+    print("My name is \(name) and age \(numberOfAge)")
+}
+
+// Перебераем массив, индекс элемента которого делится на два
+for (index, value) in array.enumerated() {
+    print(index, value)
+}
+
+// Перебор цикла через два элемента
+for i in stride(from: 0, through: 35, by: 5) {
+    print(i)
+}
+
