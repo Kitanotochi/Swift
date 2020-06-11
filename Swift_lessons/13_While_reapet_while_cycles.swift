51db52100fed47452112ebc2d967e93e58c8f159
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-11 19:20:02 +0300

    Update 13_While_reapet_while_cycles.swift

---
+++ Swift_lessons/13_While_reapet_while_cycles.swift
@@ -0,0 +1,14 @@
+// Циклы "while", "repeat while" в Swift
+
+// Пример цикла "while"
+// Этот цикл выполняется в случае, если условие верно - thrue
+var timer = 5
+print("Couting down")
+
+while timer > 0 {
+    print(timer)
+    timer -= 1
+}
+
+print("Start")
+
