eb2586beecf62178c8ae56ce8354b8c8dea1536a
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-18 19:08:28 +0300

    Update 15_Closures.swift

--- Swift_lessons/15_Closures.swift
+++ Swift_lessons/15_Closures.swift
@@ -1,2 +1,33 @@
 // Клоужеры в Swift
 
+// Клоужер - это безымянная функция
+
+// Обычный Клоужер
+let = closure {
+    print("Hello world")
+}
+
+func repeatThreeTimes(closure: () -> ()) {
+    for _ in 0...2 {
+        closure()
+    }
+}
+repeatThreeTimes(closure: closure)
+
+// Альтернативный вариант
+func repeatThreeTimes(closure: () -> ()) {
+    for _ in 0...2 {
+        closure()
+    }
+}
+    
+repeatThreeTimes(closure: {
+    print("Hello world")
+})
+
+// Более сложный пример
+let unsortedArray = [ 12, 322, 43, 98, 555, 222]
+let sortedArray = unsortedArray.sorted {
+    (number1: Int, number2: Int) -> Bool in
+    return number1 > number2
+}
