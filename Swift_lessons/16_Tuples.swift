ac9b82e07eea260618d93fdb2100c84059b647f1
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-19 19:32:56 +0300

    Update 16_Tuples.swift

--- Swift_lessons/16_Tuples.swift
+++ Swift_lessons/16_Tuples.swift
@@ -1,3 +1,20 @@
 // Кортежы в Swift
 
+// Простой картеж
+let one = 1
+let two = 2
+let three = 3
+
+(one, two, three)
+
+
+// Помещаем в одну константу два значения
+let boy = (5, "Sergei")
+// Вывод
+boy.0
+boy.1
+
+
+
+
 
