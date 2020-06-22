5a593235d4164a4791fa94e2aa0a37b44a2a558e
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-22 19:55:28 +0300

    Update 17_Optionals.swift

--- Swift_lessons/17_Optionals.swift
+++ Swift_lessons/17_Optionals.swift
@@ -3,3 +3,28 @@
 // Опционалы - это опциональные типы
 // Это такие переменные, которые могут иметь знаение равное "nil"
 
+var fuel: Int?
+fuel = 20
+fuel = nil
+// Опциональная переменная
+// Восклицательный знак, ставится для принудительного извлечения значения из переменной
+print("\(fuel!) liters left")
+
+
+// Опциональная привязка - позволяет извлечь значения без ошибок
+if let availableFuel = fuel {
+    print("\(availableFuel) liters left")
+}else {
+    print("no fuel data available")
+}
+
+
+// Создаем функцию
+func checkFuel() {
+    guard let availableFuel = fuel else {
+        print("no fuel data available")
+        return 
+    }
+    print("\(availableFuel) liters left")
+}
+checkFuel()
