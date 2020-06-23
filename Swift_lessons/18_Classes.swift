25bad1e3294ab97fdafa585241395c19f45acba1
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-23 20:24:15 +0300

    Update 18_Classes.swift

--- Swift_lessons/18_Classes.swift
+++ Swift_lessons/18_Classes.swift
@@ -1,5 +1,16 @@
 // Классы в Swift
 
+// Константы и переменные внутри класса, называются - свойствоми
+class Human {
+    var name = "Ivan"
+    var age: Int? = 30
+    var hairs = true
+    
+// Методы - это те же функции, но внутри класса
+    func description() -> String
+    
+    return "Hello, my name is\(name) and I am \(age) years old"
+}
 
 
 
