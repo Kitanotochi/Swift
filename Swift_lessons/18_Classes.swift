eeaf7a195722b890f93c328c75ddcd2ed3d4ce12
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-24 19:32:02 +0300

    Update 18_Classes.swift

--- Swift_lessons/18_Classes.swift
+++ Swift_lessons/18_Classes.swift
@@ -1,5 +1,6 @@
 // Классы в Swift
 
+// Внутри класса создаются свойства и методы
 // Константы и переменные внутри класса, называются - свойствоми
 class Human {
     var name = "Ivan"
@@ -12,5 +13,28 @@ class Human {
     return "Hello, my name is\(name) and I am \(age) years old"
 }
 
+var humanOne = Human()
+humanOne.name = "Oleg"
+humanOne.name
+humanOne.description()
+
+
+// Создаем второго человека
+// Мы можем присваивать значения для свойств экземпляров
+let humanTwo = Human()
+humanTwo.hairs = false
+humanTwo.name = "Igor"
+humanTwo
+
+// Нельзя помещать новое значение в константу
+// Можем поместить новое значение, в том случае, если это будет переменная.
+humanOne = HumanTwo
+human.name
+
+
+// Создаем массив
+var array = [Human]()
+array.append(humanOne)
+arry.append(HumanTwo)
 
 
