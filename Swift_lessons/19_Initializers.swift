7f4a87994591c8fc7aa59c02b3df8fc54a8b05a1
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-25 19:14:13 +0300

    Update 19_Initializers.swift

--- Swift_lessons/19_Initializers.swift
+++ Swift_lessons/19_Initializers.swift
@@ -19,12 +19,22 @@ class Human {
         name = "Ivan"
         hairs = true
     }
+    // Еще один инициализатор
+    // "self" определяет, что мы обращаемся к свойству
+    init(name: String, age: Int?, hairs: Bool) {
+       self.name = name
+       self.age = age
+       self.hairs = hairs
+    }
   }
 }
 
 // Инициализируем объект
 let human = Human()
 human.age 
+human.name
+human.hairs
+
 
 
 
