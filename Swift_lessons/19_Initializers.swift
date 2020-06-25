8c3f6d220b8d1e2586cfea142f9258b34fd4265e
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-25 18:54:08 +0300

    Update 19_Initializers.swift

--- Swift_lessons/19_Initializers.swift
+++ Swift_lessons/19_Initializers.swift
@@ -1,4 +1,30 @@
 // Инициализаторы в Swift
 
+// Инициализатор - это такой метод, который устанавливает начальные значения для свойств хранения
+
+class Human {
+
+    var name = String {
+    var age: Int? = 30
+    var hairs = Bool
+    
+    func description() -> String
+      if let humanAge = age {
+      return "Hello, My name is\(name) and I am \(humanAge) years old"   
+    } else {
+      return "Hello, My name is \(name)" 
+    }
+    // Создали инициализатор
+    init() {
+        name = "Ivan"
+        hairs = true
+    }
+  }
+}
+
+// Инициализируем объект
+let human = Human()
+human.age 
+
 
 
