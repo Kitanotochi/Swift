453617d93f279ff9a3e3c56bdec2327ed29f1c56
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-28 09:38:57 +0300

    Create 21_Computed_properties.swift

---
+++ Swift_lessons/21_Computed_properties.swift
@@ -0,0 +1,15 @@
+// Вычисляемые свойства в Swift
+
+// Создаем новый класс
+class Rectangular {
+    let height: Int
+    let width: Int
+    let depth: Int
+    
+    init(height: Int, width: Int, depth: Int) {
+        self.height = height
+        self.width = width
+        self.depth = depth
+    }
+}
+
