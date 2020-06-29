e576aa045f20b232cb1a196551bff58e78caff49
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-29 20:10:09 +0300

    Update 21_Computed_properties.swift

--- Swift_lessons/21_Computed_properties.swift
+++ Swift_lessons/21_Computed_properties.swift
@@ -18,3 +18,43 @@ class Rectangular {
     }
 }
 
+// Экземпляр класса
+let rect = Rectangular(height: 10, width: 12, depth: 13)
+rect.volume
+
+
+// Создаем еще один класс
+class Person {
+    var name: String
+    var secondName: String
+    
+    // Используются "геттер" и "сеттер" свойства
+    // Это блоки кода, которые определяют, что должно происходить когда вы устанавливаете и берете новое значение
+    var fullName: Srting {
+        get {
+            return name + " " + secondName
+        }
+        
+        // Использовали метод который разделяет полученную строку на элементы через пробелы
+        set(anotherNewValue) {
+            let array = anotherNewValue.components(separatedBy: " ")
+            name = array[0]
+            secondName = array[1]
+        }
+    }
+    
+    init(name: String, secondName: String) {
+        self.name = name
+        self.secondName = secondName
+        
+    }
+}
+
+let person = Person(name: "Oleg", secondName: "Romanov")
+person.fullName
+
+person.name
+person.secondName
+person.fullName = " Andrey Ivanov"
+person.name
+person.secondName
