d2dc61860d8854f69e68017058a947f4d311cf7f
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-06-30 18:25:28 +0300

    Update 22_Class Properties.swift

--- Swift_lessons/22_Class Properties.swift
+++ Swift_lessons/22_Class Properties.swift
@@ -1,2 +1,15 @@
 // Свойства классов в Swift
 
+// Создаем новый класс
+class Car {
+
+    // properties
+    let products: Int
+    let people: Int
+    let pets: Int
+    let selfWeight = 1500
+    let maxWeight = 2000
+
+    
+    
+}
