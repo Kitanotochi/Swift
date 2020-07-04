3d19f4d0837b6b107a90c6e85d1f0c4f02d0cbbc
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-07-04 19:38:41 +0300

    Update 23_Lazy_Properties.swift

--- Swift_lessons/23_Lazy_Properties.swift
+++ Swift_lessons/23_Lazy_Properties.swift
@@ -1,2 +1,18 @@
 // Ленивые свойства в Swift
 
+// Создаем внешнюю функцию, которая использует в качестве значения последнего свойства
+func bigDataProcessingFunc() -> String {
+    return "very long process"
+}
+
+// Создаем класс
+class Processing {
+    let smallDataProcessing = "small data processing"
+    let averageDataProcessing = " average data processing"
+    lazy var bigDataProcessing = bigDataProcessingFunc()
+}
+
+let process = Processing()
+process.bigDataProcessing
+process
+
