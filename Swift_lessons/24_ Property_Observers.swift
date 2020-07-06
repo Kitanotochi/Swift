de4ac1bc407f17e4c00042a6413724ba20fe9d6e
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-07-06 20:08:13 +0300

    Update 24_ Property_Observers.swift

--- Swift_lessons/24_ Property_Observers.swift
+++ Swift_lessons/24_ Property_Observers.swift
@@ -1,3 +1,35 @@
 // Наблюдатели свойств в Swift
 
+// Наблюдатели - наблюдают за изменениями значения свойств
+// Создаем новый класс
+class SecretLabEmployee {
+    
+    var accessLevel = 0 {
+        // Будет установленно
+        willSet(newValue) {
+            print("New boss is about to come!!!")
+            print("New access level is \(newValue)")
+        }
+        // Было установленно
+        didSet {
+            if accessLevel > 0 {
+                accessToDataBase = true
+            } else {
+                accessToDataBase = false
+            }
+            print("New boss just come")
+            print("Last time i had access level \(oldValue)")
+        }
+        
+    }
+    
+    var accessToDataBase = false
+}
+
+let employee = SecretLabEmployee()
+employee.accessLevel
+employee.accessToDataBase
+
+employee.accessLevel = 1
+employee.accessToDataBase
 
