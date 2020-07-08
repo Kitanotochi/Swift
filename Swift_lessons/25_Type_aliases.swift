58d3899005dfffba660799cf5212c5a4ba979971
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-07-08 19:12:56 +0300

    Update 25_Type_aliases.swift

--- Swift_lessons/25_Type_aliases.swift
+++ Swift_lessons/25_Type_aliases.swift
@@ -1,4 +1,30 @@
 // Алиасы типов в Swift
 
+// Псевдонимы типов
+
+
+// Пример
+typealias Meter = Int
+
+let length: Meter = 50
+
+let length1 = 20
+
+let sum: Meter = length + length1
+
+// Пример
+// Записали кортеж
+typealias DoubleInteger = (Int, Int)
+
+let someConstant: DoubleInteger = (1, 2)
+
+//Пример
+// Заменяем тип словаря
+typealias DictionaryType = [String: Int]
+
+var dictionary: DictionaryType = [:]()
+dictionary["Apartment"] = 777
+dictionary
+
 
 
