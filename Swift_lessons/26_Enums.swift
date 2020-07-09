f1a96f3ca9fa8d577bc6827944d00c8b4ecb983a
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-07-09 19:07:33 +0300

    Update 26_Enums.swift

--- Swift_lessons/26_Enums.swift
+++ Swift_lessons/26_Enums.swift
@@ -1,3 +1,46 @@
 // Энумы в Swift
 
+// Типы первого класса, объединяющие в себе связанные значения и присваивают им один общий тип 
+// При работе с энумами не возможно выйти за рамки этого типа 
+// Энумы содержат свойства и методы
+
+// Пример
+enum Movement: String {
+    // "Case" основная состовляющая часть "энума"
+    case forward
+    case backward
+    case left
+    case right
+}
+
+let movementDirection = Movement.backward.rawValue
+
+
+// Пример
+enum Movement: Int {
+    // "Case" основная состовляющая часть "энума"
+    case forward = 35
+    case backward = 10
+    case left = 40
+    case right = 50
+}
+
+let movementDirection = Movement.backward.rawValue
+
+
+// Пример
+// Энум содержащий два кейса и одно вычесляемое свойство
+enum Device {
+    case iPad, iPhone
+    
+    var year: Int {
+        switch self {
+        case .iPhone: return 2007
+        case .iPad: return 2020
+        }
+    }
+}
+
+let yearOfProduction = Device.iPad.year
+let yearOfProduction = Device.iPhone.year
 
