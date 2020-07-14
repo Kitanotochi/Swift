16881b521f7372246c65332086348a77f841fc65
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-07-14 19:15:52 +0300

    Update 27_Structures.swift

--- Swift_lessons/27_Structures.swift
+++ Swift_lessons/27_Structures.swift
@@ -1,4 +1,26 @@
 // Структуры в Swift
 
+// Структуры в Swift
+
+// Простая структура / Класс
+class Site {
+    // Сохраненные свойства
+    var siteName = "SwiftBook.ru"
+    let visitsToday = 1000
+    let visitsYesterday = 1000
+    
+    // Вычисляемые свойства
+    var visitsTommorow: Int {
+        return (visitsToday + visitsYesterday) / 2
+    }
+    
+    // Ленивые свойства
+    lazy var someLazyProperty = 1 + 2
+    
+    func description() -> String {
+        return ("We had \(visitsYesterday) visitors yesterday. Today we have \(visitsToday) visitors. Tommorow we will have about \(visitsTommorow) visitors.")
+    }
+}
+
 
 
