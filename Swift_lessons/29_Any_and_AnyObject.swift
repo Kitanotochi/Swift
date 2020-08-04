4c2f7e8da698c1116482b3c9824941101fdc70db
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-08-04 20:12:17 +0300

    Update 29_Any_and_AnyObject.swift

--- Swift_lessons/29_Any_and_AnyObject.swift
+++ Swift_lessons/29_Any_and_AnyObject.swift
@@ -1,3 +1,34 @@
 // Типы Any and AnyObject в Swift
 
+class A { // Класс A
+    
+}
+
+class B { // Класс B
+    
+}
+
+class C { // Класс C
+    
+}
+
+let a = A() // Экземпляр класса А
+let b = A() // Экземпляр класса A
+let c = A() // Экземпляр класса A
+let d = B() // Экземпляр класса B
+let e = C() // Экземпляр класса C
+
+struct D { // Структура D
+    
+}
+
+let f = D() // Экземпляр структуры
+
+enum E { // Энум
+    case a
+    case b 
+}
+
+let g = E.b // Экземпляр энума
+
 
